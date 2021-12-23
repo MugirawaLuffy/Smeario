e90ad162fcd5e9adcf2290ba5adaf1fb5f82f628
Nils Drees <herrnielsson.nd@gmail.com>
2021-12-23 13:01:18 +0100

    bugfixes

--- src/main/java/physics2d/rigidbody/IntersectionDetector2D.java
+++ src/main/java/physics2d/rigidbody/IntersectionDetector2D.java
@@ -1,4 +1,5 @@
 package physics2d.rigidbody;
+
 import org.joml.Vector2f;
 import physics2d.primitives.*;
 import renderer.Line2D;
@@ -49,6 +50,7 @@ public class IntersectionDetector2D {
         return pointLocalBoxSpace.x <= max.x && min.x <= pointLocalBoxSpace.x &&
                 pointLocalBoxSpace.y <= max.y && min.y <= pointLocalBoxSpace.y;
     }
+
     // ========================================================
     // Line vs. Primitive Tests
     // ========================================================
@@ -241,5 +243,163 @@ public class IntersectionDetector2D {
         }
 
         return true;
+    }
+
+    // =============================================================================
+    // Circle vs. Primitive tests
+    // =============================================================================
+    public static boolean circleAndLine(Circle circle, Line2D line) {
+        return lineAndCircle(line, circle);
+    }
+
+    public static boolean circleAndCircle(Circle c1, Circle c2) {
+        Vector2f vecBetweenCenters = new Vector2f(c1.getCenter()).sub(c2.getCenter());
+        float radiiSum = c1.getRadius() + c2.getRadius();
+        return vecBetweenCenters.lengthSquared() <= radiiSum * radiiSum;
+    }
+
+    public static boolean circleAndAABB(Circle circle, AABB box) {
+        Vector2f min = box.getMin();
+        Vector2f max = box.getMax();
+
+        Vector2f closestPointToCircle = new Vector2f(circle.getCenter());
+        if (closestPointToCircle.x < min.x) {
+            closestPointToCircle.x = min.x;
+        } else if (closestPointToCircle.x > max.x) {
+            closestPointToCircle.x = max.x;
+        }
+
+        if (closestPointToCircle.y < min.y) {
+            closestPointToCircle.y = min.y;
+        } else if (closestPointToCircle.y > max.y) {
+            closestPointToCircle.y = max.y;
+        }
+
+        Vector2f circleToBox = new Vector2f(circle.getCenter()).sub(closestPointToCircle);
+        return circleToBox.lengthSquared() <= circle.getRadius() * circle.getRadius();
+    }
+
+    public static boolean circleAndBox2D(Circle circle, Box2D box) {
+        // Treat the box just like an AABB, after we rotate the stuff
+        Vector2f min = new Vector2f();
+        Vector2f max = new Vector2f(box.getHalfSize()).mul(2.0f);
+
+        // Create a circle in box's local space
+        Vector2f r = new Vector2f(circle.getCenter()).sub(box.getRigidbody().getPosition());
+        JMath.rotate(r, -box.getRigidbody().getRotation(), new Vector2f());
+        Vector2f localCirclePos = new Vector2f(r).add(box.getHalfSize());
+
+        Vector2f closestPointToCircle = new Vector2f(localCirclePos);
+        if (closestPointToCircle.x < min.x) {
+            closestPointToCircle.x = min.x;
+        } else if (closestPointToCircle.x > max.x) {
+            closestPointToCircle.x = max.x;
+        }
+
+        if (closestPointToCircle.y < min.y) {
+            closestPointToCircle.y = min.y;
+        } else if (closestPointToCircle.y > max.y) {
+            closestPointToCircle.y = max.y;
+        }
+
+        Vector2f circleToBox = new Vector2f(localCirclePos).sub(closestPointToCircle);
+        return circleToBox.lengthSquared() <= circle.getRadius() * circle.getRadius();
+    }
+
+    // =============================================================================
+    // AABB vs. Primitive tests
+    // =============================================================================
+    public static boolean AABBAndCircle(AABB box, Circle circle) {
+        return circleAndAABB(circle, box);
+    }
+
+    public static boolean AABBAndAABBB(AABB b1, AABB b2) {
+        Vector2f axesToTest[] = {new Vector2f(0, 1), new Vector2f(1, 0)};
+        for (int i=0; i < axesToTest.length; i++) {
+            if (!overlapOnAxis(b1, b2, axesToTest[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean AABBAndBox2D(AABB b1, Box2D b2) {
+        Vector2f axesToTest[] = {
+                new Vector2f(0, 1), new Vector2f(1, 0),
+                new Vector2f(0, 1), new Vector2f(1, 0)
+        };
+        JMath.rotate(axesToTest[2], b2.getRigidbody().getRotation(), new Vector2f());
+        JMath.rotate(axesToTest[3], b2.getRigidbody().getRotation(), new Vector2f());
+        for (int i=0; i < axesToTest.length; i++) {
+            if (!overlapOnAxis(b1, b2, axesToTest[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // =============================================================================
+    // SAT helpers
+    // =============================================================================
+    private static boolean overlapOnAxis(AABB b1, AABB b2, Vector2f axis) {
+        Vector2f interval1 = getInterval(b1, axis);
+        Vector2f interval2 = getInterval(b2, axis);
+        return ((interval2.x <= interval1.y) && (interval1.x <= interval2.y));
+    }
+
+    private static boolean overlapOnAxis(AABB b1, Box2D b2, Vector2f axis) {
+        Vector2f interval1 = getInterval(b1, axis);
+        Vector2f interval2 = getInterval(b2, axis);
+        return ((interval2.x <= interval1.y) && (interval1.x <= interval2.y));
+    }
+
+    private static boolean overlapOnAxis(Box2D b1, Box2D b2, Vector2f axis) {
+        Vector2f interval1 = getInterval(b1, axis);
+        Vector2f interval2 = getInterval(b2, axis);
+        return ((interval2.x <= interval1.y) && (interval1.x <= interval2.y));
+    }
+
+    private static Vector2f getInterval(AABB rect, Vector2f axis) {
+        Vector2f result = new Vector2f(0, 0);
+
+        Vector2f min = rect.getMin();
+        Vector2f max = rect.getMax();
+
+        Vector2f vertices[] = {
+                new Vector2f(min.x, min.y), new Vector2f(min.x, max.y),
+                new Vector2f(max.x, min.y), new Vector2f(max.x, max.y)
+        };
+
+        result.x = axis.dot(vertices[0]);
+        result.y = result.x;
+        for (int i=1; i < 4; i++) {
+            float projection = axis.dot(vertices[i]);
+            if (projection < result.x) {
+                result.x = projection;
+            }
+            if (projection > result.y) {
+                result.y = projection;
+            }
+        }
+        return result;
+    }
+
+    private static Vector2f getInterval(Box2D rect, Vector2f axis) {
+        Vector2f result = new Vector2f(0, 0);
+
+        Vector2f vertices[] = rect.getVertices();
+
+        result.x = axis.dot(vertices[0]);
+        result.y = result.x;
+        for (int i=1; i < 4; i++) {
+            float projection = axis.dot(vertices[i]);
+            if (projection < result.x) {
+                result.x = projection;
+            }
+            if (projection > result.y) {
+                result.y = projection;
+            }
+        }
+        return result;
     }
 }
--- src/main/java/renderer/Texture.java
+++ src/main/java/renderer/Texture.java
@@ -13,49 +13,56 @@ public class Texture {
     private int texID;
     private int width, height;
 
+//    public Texture(String filepath) {
+//
+//    }
+
     public void __init__(String filepath) {
         this.filepath = filepath;
-        //Generate texture on gpu
+
+        // Generate texture on GPU
         texID = glGenTextures();
         glBindTexture(GL_TEXTURE_2D, texID);
 
-        //Set texture params
-        //Repeat texture
+        // Set texture parameters
+        // Repeat image in both directions
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-        //When stretching/shrinking, pixelate for NES looook
+        // When stretching the image, pixelate
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        // When shrinking an image, pixelate
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-        //load image
         IntBuffer width = BufferUtils.createIntBuffer(1);
         IntBuffer height = BufferUtils.createIntBuffer(1);
         IntBuffer channels = BufferUtils.createIntBuffer(1);
         stbi_set_flip_vertically_on_load(true);
         ByteBuffer image = stbi_load(filepath, width, height, channels, 0);
 
-        if(image != null){
+        if (image != null) {
             this.width = width.get(0);
             this.height = height.get(0);
-            if(channels.get(0) == 3) {
+
+            if (channels.get(0) == 3) {
                 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width.get(0), height.get(0),
                         0, GL_RGB, GL_UNSIGNED_BYTE, image);
-            } else if(channels.get(0) == 4) {
+            } else if (channels.get(0) == 4) {
                 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width.get(0), height.get(0),
                         0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-            } else assert false : "unknown number of channels in texture '" + filepath + "': " +
-                    channels.get(0) + " channels";
-
+            } else {
+                assert false : "Error: (Texture) Unknown number of channesl '" + channels.get(0) + "'";
+            }
         } else {
-            assert false : "Error: Texture could not load image '" + filepath + "'.";
+            assert false : "Error: (Texture) Could not load image '" + filepath + "'";
         }
+
         stbi_image_free(image);
     }
 
     public void bind() {
         glBindTexture(GL_TEXTURE_2D, texID);
     }
+
     public void unbind() {
         glBindTexture(GL_TEXTURE_2D, 0);
     }
@@ -64,10 +71,24 @@ public class Texture {
         return this.width;
     }
 
+    public String getFilepath() {
+        return this.filepath;
+    }
+
     public int getHeight() {
         return this.height;
     }
+
     public int getId() {
-        return this.texID;
+        return texID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (!(o instanceof Texture)) return false;
+        Texture oTex = (Texture)o;
+        return oTex.getWidth() == this.width && oTex.getHeight() == this.height && oTex.getId() == this.texID &&
+                oTex.getFilepath().equals(this.filepath);
     }
 }
